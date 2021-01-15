005b4b4a417b305a5cfc2cfd30f531d2adbccbd3
gabriel-smc <gabrieltnguyen@gmail.com>
2021-01-15 17:16:06 +0700

    modified config, added config backup

--- src/demo/config-bk.tsx
+++ src/demo/config-bk.tsx
@@ -215,6 +215,44 @@ export default skin => {
   //////////////////////////////////////////////////////////////////////
 
   const fields: Fields = {
+    user: {
+      label: "User",
+      tooltip: "Group of fields",
+      type: "!struct",
+      subfields: {
+        firstName: {
+          label2: "Username", //only for menu's toggler
+          type: "text",
+          excludeOperators: ["proximity", "like"],
+          fieldSettings: {
+            validateValue: (val, fieldSettings) => {
+              return val.length < 10;
+            }
+          },
+          mainWidgetProps: {
+            valueLabel: "Name",
+            valuePlaceholder: "Enter name"
+          }
+        },
+        login: {
+          type: "text",
+          tableName: "t1", // PR #18, PR #20
+          excludeOperators: ["proximity"],
+          fieldSettings: {
+            validateValue: (val, fieldSettings) => {
+              return (
+                val.length < 10 &&
+                (val === "" || val.match(/^[A-Za-z0-9_-]+$/) !== null)
+              );
+            }
+          },
+          mainWidgetProps: {
+            valueLabel: "Login",
+            valuePlaceholder: "Enter login"
+          }
+        }
+      }
+    },
     appointment: {
       label: "Appointment",
       type: "!group",
@@ -358,6 +396,209 @@ export default skin => {
           },
           valueSources: ["value"]
         }
+      }
+    },
+    prox1: {
+      label: "prox",
+      tooltip: "Proximity search",
+      type: "number",
+      operators: ["proximity"]
+    },
+    num: {
+      label: "Number",
+      type: "number",
+      preferWidgets: ["number"],
+      fieldSettings: {
+        min: -1,
+        max: 5
+      },
+      funcs: ["LINEAR_REGRESSION"]
+    },
+    slider: {
+      label: "Slider",
+      type: "number",
+      preferWidgets: ["slider", "rangeslider"],
+      valueSources: ["value", "field"],
+      fieldSettings: {
+        min: 0,
+        max: 100,
+        step: 1,
+        marks: {
+          0: <strong>0%</strong>,
+          100: <strong>100%</strong>
+        },
+        validateValue: (val, fieldSettings) => {
+          return val < 50 ? null : "Invalid slider value, see validateValue()";
+        }
+      },
+      //overrides
+      widgets: {
+        slider: {
+          widgetProps: {
+            valuePlaceholder: "..Slider"
+          }
+        },
+        rangeslider: {
+          widgetProps: {
+            valueLabels: [
+              { label: "Number from", placeholder: "from" },
+              { label: "Number to", placeholder: "to" }
+            ]
+          }
+        }
+      }
+    },
+    date: {
+      label: "Date",
+      type: "date",
+      valueSources: ["value"],
+      fieldSettings: {
+        dateFormat: "DD-MM-YYYY",
+        validateValue: (val, fieldSettings: DateTimeFieldSettings) => {
+          // example of date validation
+          const dateVal = moment(val, fieldSettings.valueFormat);
+          return dateVal.year() !== new Date().getFullYear()
+            ? "Please use current year"
+            : null;
+        }
+      }
+    },
+    time: {
+      label: "Time",
+      type: "time",
+      valueSources: ["value"],
+      defaultOperator: "between"
+    },
+    datetime: {
+      label: "DateTime",
+      type: "datetime",
+      valueSources: ["value"]
+    },
+    datetime2: {
+      label: "DateTime2",
+      type: "datetime",
+      valueSources: ["field"]
+    },
+    color: {
+      label: "Color",
+      type: "select",
+      valueSources: ["value"],
+      fieldSettings: {
+        // * old format:
+        // listValues: {
+        //     yellow: 'Yellow',
+        //     green: 'Green',
+        //     orange: 'Orange'
+        // },
+        // * new format:
+        listValues: [
+          { value: "yellow", title: "Yellow" },
+          { value: "green", title: "Green" },
+          { value: "orange", title: "Orange" }
+        ]
+      }
+    },
+    color2: {
+      label: "Color2",
+      type: "select",
+      fieldSettings: {
+        listValues: {
+          yellow: "Yellow",
+          green: "Green",
+          orange: "Orange",
+          purple: "Purple"
+        }
+      }
+    },
+    multicolor: {
+      label: "Colors",
+      type: "multiselect",
+      fieldSettings: {
+        listValues: {
+          yellow: "Yellow",
+          green: "Green",
+          orange: "Orange"
+        },
+        allowCustomValues: true
+      }
+    },
+    selecttree: {
+      label: "Color (tree)",
+      type: "treeselect",
+      fieldSettings: {
+        treeExpandAll: true,
+        // * deep format (will be auto converted to flat format):
+        // listValues: [
+        //     { value: "1", title: "Warm colors", children: [
+        //         { value: "2", title: "Red" },
+        //         { value: "3", title: "Orange" }
+        //     ] },
+        //     { value: "4", title: "Cool colors", children: [
+        //         { value: "5", title: "Green" },
+        //         { value: "6", title: "Blue", children: [
+        //             { value: "7", title: "Sub blue", children: [
+        //                 { value: "8", title: "Sub sub blue and a long text" }
+        //             ] }
+        //         ] }
+        //     ] }
+        // ],
+        // * flat format:
+        listValues: [
+          { value: "1", title: "Warm colors" },
+          { value: "2", title: "Red", parent: "1" },
+          { value: "3", title: "Orange", parent: "1" },
+          { value: "4", title: "Cool colors" },
+          { value: "5", title: "Green", parent: "4" },
+          { value: "6", title: "Blue", parent: "4" },
+          { value: "7", title: "Sub blue", parent: "6" },
+          { value: "8", title: "Sub sub blue and a long text", parent: "7" }
+        ]
+      }
+    },
+    multiselecttree: {
+      label: "Colors (tree)",
+      type: "treemultiselect",
+      fieldSettings: {
+        treeExpandAll: true,
+        listValues: [
+          {
+            value: "1",
+            title: "Warm colors",
+            children: [
+              { value: "2", title: "Red" },
+              { value: "3", title: "Orange" }
+            ]
+          },
+          {
+            value: "4",
+            title: "Cool colors",
+            children: [
+              { value: "5", title: "Green" },
+              {
+                value: "6",
+                title: "Blue",
+                children: [
+                  {
+                    value: "7",
+                    title: "Sub blue",
+                    children: [
+                      { value: "8", title: "Sub sub blue and a long text" }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    },
+    stock: {
+      label: "In stock",
+      type: "boolean",
+      defaultValue: true,
+      mainWidgetProps: {
+        labelYes: "+",
+        labelNo: "-"
       }
     }
   };
